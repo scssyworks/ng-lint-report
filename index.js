ddf089592a21faa37becabdd8c1639dc31b1a6df
Sachin Singh <ssingh259@sapient.com>
2020-05-23 16:10:59 +0530

    #1 Fixed issue with failure message containing $ symbol along with apostrophe breaking the HTML

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const jsonOutputPath = argv.jsonfile;
 const reportOutputPath = argv.output || `ng-lint-report`;
 const outputFileName = argv.filename || 'report.html';
 
+function sanitize(str) {
+    str = str.replace(/['"$]/g, (match) => {
+        if (match === `'`) return `&apos;`;
+        if (match === `"`) return `&quot;`;
+        if (match === '$') return `&#36;`;
+        return match;
+    });
+    return str;
+}
+
 if (typeof jsonOutputPath === 'string') {
     const htmlTemplate = fs.readFileSync(`${rootDir}/src/index.html`, 'utf8');
     try {
@@ -43,9 +53,9 @@ if (typeof jsonOutputPath === 'string') {
                 jsonMapKeys.forEach(filename => {
                     html += `<button class="file-name clearfix">${filename} <em class="float-right">(${jsonMap[filename].length} issue(s) found) <span>+</span></em></button>`;
                     html += `<div class="result-wrap">
-                <ul><li class="item-head"><span>Path</span><span>Rule</span><span>Error</span><span>Position</span></li>${jsonMap[filename].map(item => {
-                        return `<li title="${item.path}" class="item is-${item.type.toLowerCase()}">
-                    <span class="item-cell failure">${item.failure}</span>
+                <ul><li class="item-head"><span>Description</span><span>Rule</span><span>Error</span><span>Position</span></li>${jsonMap[filename].map(item => {
+                        return `<li title="${encodeURIComponent(item.path)}" class="item is-${item.type.toLowerCase()}">
+                    <span class="item-cell failure">${sanitize(item.failure)}</span>
                     <span class="item-cell rule">${item.rule}</span>
                     <span class="item-cell type">${item.type}</span>
                     <span class="item-cell position">${item.position}</span>
@@ -59,7 +69,11 @@ if (typeof jsonOutputPath === 'string') {
             const outputFolders = path.join(currentWorkingDir, reportOutputPath);
             if (!fs.existsSync(outputFolders))
                 fs.mkdirsSync(outputFolders);
-            fs.writeFileSync(path.join(outputFolders, outputFileName), htmlTemplate.replace('#{placeholder}', `<div class="result-outer-wrap">${html}</div>`));
+            fs.writeFileSync(
+                path.join(outputFolders, outputFileName),
+                htmlTemplate.replace(/##placeholder##/, `<div class="result-outer-wrap">${html.trim()}</div>`),
+                'utf8'
+            );
         } else {
             throw new Error('JSON format is not valid');
         }
